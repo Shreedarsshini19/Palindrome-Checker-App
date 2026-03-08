cfef72ab1e92eebb3e894f80f3c8e73f330a059f
Shreedarsshini19 <197891326+Shreedarsshini19@users.noreply.github.com>
2026-03-08 17:50:16 +0530

    Add UC1 - Welcome message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class UseCase1PalindromeCheckerApp{
+    public static void main(String[] args){
+        System.out.println("========================");
+        System.out.println("Welcome to the Palindrome Checker App!");
+        System.out.println("Version:1.0");
+        System.out.println("This app will help you check if a string is a palindrome.");
+        System.out.println("==================================");
+        System.out.println("Application startup complete. Ready for next use case.");
+    }
+}
