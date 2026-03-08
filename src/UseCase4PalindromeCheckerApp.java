3197cc59e96b2be59d8472bdaab43c11b616760d
Shreedarsshini19 <197891326+Shreedarsshini19@users.noreply.github.com>
2026-03-08 18:45:32 +0530

    Add UC4- Chracter array based palindrome check

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String word = "level";
+        char[] chars = word.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a palindrome.");
+        }
+        System.out.println("Program finished.");
+    }
+}
