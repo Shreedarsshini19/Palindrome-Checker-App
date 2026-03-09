7d4e254ec7be3f0d12a3d5277aa7b05c19902d3b
Shreedarsshini19 <sridharshnisai@gmail.com>
2026-03-09 15:31:18 +0530

    Character Array Based Palindrome Check

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,15 +1,23 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-        String word = "racecar";
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+        String word = "level";
+        char[] chars = word.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if (word.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("The word \"" + word + "\" is a palindrome.");
         } else {
             System.out.println("The word \"" + word + "\" is NOT a palindrome.");
         }
         System.out.println("Program finished.");
-        }
     }
+}
