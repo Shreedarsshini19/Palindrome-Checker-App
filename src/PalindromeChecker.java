12701d2693cce4d887d9b2cd3dad70cf092e06f1
Shreedarsshini19 <197891326+Shreedarsshini19@users.noreply.github.com>
2026-03-16 14:49:11 +0530

    Strategy Pattern for Palindrome Algorithms

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,39 +1,66 @@
-import java.util.Scanner;
+import java.util.*;
 public class PalindromeChecker {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        PalindromeService service = new PalindromeService();
 
-        boolean result = service.checkPalindrome(input);
+        System.out.println("Choose Strategy:");
+        System.out.println("1. Stack Strategy");
+        System.out.println("2. Deque Strategy");
+
+        int choice = sc.nextInt();
+
+        PalindromeStrategy strategy;
+
+        if (choice == 1) {
+            strategy = new StackStrategy();
+        } else {
+            strategy = new DequeStrategy();
+        }
+
+        boolean result = strategy.checkPalindrome(input);
 
         if (result) {
-            System.out.println("Is Palindrome: true");
+            System.out.println("Palindrome");
         } else {
-            System.out.println("Is Palindrome: false");
+            System.out.println("Not Palindrome");
         }
 
         sc.close();
     }
 }
-
-class PalindromeService {
+interface PalindromeStrategy {
+    boolean checkPalindrome(String input);
+}
+class StackStrategy implements PalindromeStrategy {
     public boolean checkPalindrome(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
 
-        int start = 0;
-        int end = input.length() - 1;
+        return input.equals(reversed);
+    }
+}
+class DequeStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
 
-        while (start < end) {
-
-            if (input.charAt(start) != input.charAt(end)) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 return false;
             }
-
-            start++;
-            end--;
         }
 
         return true;
